a96ace7c6709ff3bff52716485615438ac4f45b7
KateLis84 <emma12022002@gmail.com>
2021-11-26 14:25:25 +0200

    Added MaterialUI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button'
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
 
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello World!!!</h1>
