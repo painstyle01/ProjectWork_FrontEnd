6dbaaacf8db37e861ed25417d8eaf787450567b4
KateLis84 <emma12022002@gmail.com>
2021-12-01 15:48:04 +0200

    sort files in folders

--- src/modules/app/App.js
+++ src/modules/app/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-import Button from '@mui/material/Button'
 import {ThemeProvider} from '@mui/material/styles'
-import MainTheme from './themes/mainThemes'
+import MainTheme from '../themes/mainThemes'
+import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
-
-
-
-
-
-
-
 
 
 
 function App() {
   return (
     <ThemeProvider theme={MainTheme}>
-      
     </ThemeProvider>
     
   );
