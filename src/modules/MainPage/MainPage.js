d73db4bc21e8ee3234feda134a5f373d241c4a25
MarinaZak <marinna27.01.2002@gmail.com>
2021-12-09 21:16:30 +0200

    .

---
+++ src/modules/MainPage/MainPage.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './MainPage.css';
+import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
+
+
+function MainPage() {
+        return (
+            <div>
+                <Header/>
+                <p>Some content</p>
+                <p>Some content</p>
+                <p>Some content</p>
+                <p>Some content</p>
+                <p>Some content</p>
+                <p>Some content</p>
+                <p>Some content</p>
+                <p>Some content</p>
+                <p>Some content</p>
+                <p>Some content</p>
+                <p>Some content</p>
+                <p>Some content</p>
+                <p>Some content</p>
+                <p>Some content</p>
+                <p>Some content</p>
+                <p>Some content</p>
+                <p>Some content</p>
+                <p>Some content</p>
+                <p>Some content</p>
+                <p>Some content</p>
+                <Footer/>
+            </div>
+        )
+}
+
+export default MainPage;
