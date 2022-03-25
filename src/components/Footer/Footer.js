5a98ab2f31c1ead3078c4dc71d152c72a9a0e20b
MarinaZak <marinna27.01.2002@gmail.com>
2022-03-25 18:54:06 +0200

    .

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,25 +1,70 @@
 import React from 'react';
 import './Footer.css';
-import AppBar from '@mui/material/AppBar'
 import Typography from '@mui/material/Typography'
-import Container from '@mui/material/Container'
-import { Box } from '@mui/system';
-import FacebookIcon from '@mui/icons-material/Facebook';
+import { Stack } from '@mui/material';
+import Divider from '@mui/material/Divider';
+import Grid from '@mui/material/Grid'
 import InstagramIcon from '@mui/icons-material/Instagram';
 import YouTubeIcon from '@mui/icons-material/YouTube';
-import logo from '../../images/logo.png'
-import logo2 from '../../images/logo3.png'
-import Grid from '@mui/material/Grid'
-import Stack from '@mui/material/Stack';
-
-
+import az from '../../images/logo2.png'
+import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp';
 
 
 function Footer() {
         return (
-        <AppBar position="static" color="primary" className="footer" >
-        <Container maxWidth="lg" sx={{marginY: '50px'}}>
-        <Grid container spacing={2}>
+        <div className="footer">
+          <Stack direction="row" spacing={2}>
+            <div style={{fontSize: '71px', lineHeight: '50px'}}>Спланувати візит</div>
+            <ArrowCircleUpIcon sx={{fontSize: '65px', transform: 'rotate(45deg)'}}/>
+          </Stack>
+          <Divider color='black' style={{borderBottomWidth: 4, marginBlock: '30px'}}/>
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={4} lg={2} sx={{color: 'black'}}>
+              <Typography variant="h5" component="div" color='primary' style={{fontWeight: 'bold', marginBottom: '10px'}}>
+                Локація
+              </Typography>
+              <Typography variant="body2" component="div" color='primary'>
+                вул. Івана Франка, 150-152, м. Львів 79011, Україна
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sm={4} lg={2}>
+              <Typography variant="h5" component="div" color='primary' style={{fontWeight: 'bold', marginBottom: '10px'}}>
+                Зв'язок з нами
+              </Typography>
+              <Typography variant="body2" component="div" color='primary'>
+                +38 096 15 84 606<br/>lviv_franko@ukr.net
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sm={4} lg={2}>
+              <Typography variant="h5" component="div" color='primary' style={{fontWeight: 'bold', marginBottom: '10px'}}>
+                Час роботи
+              </Typography>
+              <Typography variant="body2" component="div" color='primary'>
+                Чекаємо на Вас щодня, крім вівторка, з 10:00 до 17:00
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sm={4} lg={2}>
+              <Typography variant="h5" component="div" color='primary' style={{fontWeight: 'bold', marginBottom: '10px'}}>
+                Соцмережі
+              </Typography>
+              <Stack direction="row" spacing={2}>
+                <a className="linkIcon" href="https://www.instagram.com/dim_franka/"><InstagramIcon sx={{fontSize: '50px'}}/></a>
+                <a className="linkIcon" href="https://www.youtube.com/channel/UC6UeOsivTNCowjVt3Hg5G0g"><YouTubeIcon sx={{fontSize: '50px'}}/></a>
+              </Stack>
+            </Grid>
+            <Grid item xs={12} sm={8} lg={4} sx={{alignSelf: 'center'}}>
+              <Stack direction="row" spacing={2} sx={{float: 'right'}} id='pdtrm'>
+              <Typography variant="h4" component="div" color='primary' style={{fontWeight: 'bold'}}>
+                Підтримай нас
+              </Typography>
+              <ArrowCircleUpIcon sx={{fontSize: '45px', transform: 'rotate(45deg)'}}/>
+              </Stack>
+            </Grid>
+          </Grid>
+          <Divider color='black' style={{borderBottomWidth: 4, marginBlock: '30px'}}/>
+          <img src={az}></img>
+          
+        {/* <Grid container spacing={2}>
           <Grid item xs={12} sm={6} md={3} sx={{marginBottom: '50px'}}>
             <Box>
             <Typography variant="h6" component="div">
@@ -88,9 +133,8 @@ function Footer() {
               </Typography>
             </Box>
           </Grid>
-        </Grid>
-        </Container>
-        </AppBar>
+        </Grid> */}
+        </div>
         )
 }
 
--- src/modules/MainPage/MainPage.js
+++ src/modules/MainPage/MainPage.js
@@ -8,62 +8,126 @@ import WatchLaterIcon from '@mui/icons-material/WatchLater';
 import newsImage from '../../images/news.png'
 import Grid from '@mui/material/Grid'
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp';
+import family from '../../images/image 14.png'
+import room from '../../images/image 9.png'
+import franko from '../../images/image 12.png'
 
 
 function MainPage() {
     let news = [
-        {'title': 'Легенда про вічне життя', 'text': 'У рамках реалізації проєкту Франко. Re:volution Львівського національного літературно-меморіального музею...'},
-        {'title': 'Легенда про вічне життя', 'text': 'У рамках реалізації проєкту Франко. Re:volution Львівського національного літературно-меморіального музею...'},
-        {'title': 'Легенда про вічне життя', 'text': 'У рамках реалізації проєкту Франко. Re:volution Львівського національного літературно-меморіального музею...'},
+        {'title': 'Легенда про вічне життя', 'date': '12 Червня 2021'},
+        {'title': 'Легенда про вічне життя', 'date': '12 Червня 2021'},
+        {'title': 'Легенда про вічне життя', 'date': '12 Червня 2021'}
     ]
         return (
-            <div className="main">
+            <div>
                 <div className="top">
                     {/* <video id="background-video" autoPlay loop muted poster="https://assets.codepen.io/6093409/river.jpg">
                         <source src="https://assets.codepen.io/6093409/river.mp4" type="video/mp4"></source>
                         Your browser does not support the video tag.
                     </video> */}
                     <div className="top_content">
-                        <div className="mainTitle">Дім Франка</div>
+                        <div className="mainTitle">ДІМ ФРАНКА</div>
                         {/* <Typography variant="test" color="secondary">Дім Франка</Typography> */}
-                        <Stack direction="row" justifyContent='space-between' marginBlock={'1vw'} spacing={2}>
-                        <Typography variant="body2" color="secondary" width="110px">Музей Франка і не тільки...</Typography>
-                            <Stack direction="row" spacing={2}>
-                            <Button variant="text" color="secondary" id='but' className='b2'>
-                                Спланувати візит
-                            </Button>
-                            <Button variant="text" color="secondary" id='but'>
-                                Підтримати нас
-                            </Button>
-                            </Stack>
+                        <Typography variant="body2" color="secondary" marginBlock={'30px'} sx={{textAlign: 'center'}}>Ласкаво просимо до музею Франка!</Typography>
+                        <Stack direction="row" spacing={2} sx={{width: '100%', justifyContent: 'center'}}>
+                        <Button variant="text" color="secondary" id='but'>
+                            Підтримай нас
+                        </Button>
+                        <Button variant="text" color="secondary" id='but' className='b2'>
+                            Спланувати візит
+                        </Button>
                         </Stack>
-                        <Divider color='white' sx={{borderBottomWidth: 4, marginBottom: '1vw'}} />
-                        <Stack direction="row">
-                        <WatchLaterIcon sx={{color: 'white', marginRight: '10px'}}/>
-                        <Typography variant="body2" color="secondary" fontWeight="bold">Чекаємо на Вас щодня, крім вівторка, з 10:00 до 17:00</Typography>
+                    </div>
+                </div>
+                <div className="middle">
+                    <div className="middle_content">
+                        <Typography variant="body2" color="secondary" style={{fontSize: '3vw', width: '80%'}}>Дім Франка – це не просто Музей. Це також Дім Муз, де вирує багатогранне мистецьке життя; Дім Науки, що є осередком франкознавчих та музеологічних студій;</Typography>
+                        <Stack direction="row" spacing={2} marginTop={'3vw'} justifyContent={'space-between'}>
+                            <Stack marginTop={'1vw'}>
+                                <a className="white_link" href='https://www.youtube.com/channel/UC6UeOsivTNCowjVt3Hg5G0g'><Typography variant="body2" color="secondary">YouTube</Typography></a>
+                                <a className="white_link" href='https://www.instagram.com/dim_franka/'><Typography variant="body2" color="secondary">Instagram</Typography></a>
+                            </Stack>
+                            <Typography variant="body2" color="secondary" style={{fontSize: '3vw', width: '80%'}}>Дім Діалогу, де відбувається чесна дискусія та пошук порозуміння; Дім Родини, де по-домашньому затишно і тепло. А ще це справжнє Місце Сили. Ласкаво просимо в гості до живого творця і "цілого чоловіка"! Тут ФРАНКОВО!</Typography>
                         </Stack>
                     </div>
                 </div>
                 <div className="bottom">
-                    <div className="bottom_content">
-                        <div style={{fontSize: '71px', lineHeight: '50px'}}>Останні новини</div>
-                        <Divider color='black' sx={{borderBottomWidth: 4, marginBlock: '30px'}} />
+                    <div className="bottom_content" style={{paddingBottom: '0'}}>
+                        <Stack direction="row" spacing={2}>
+                            <div style={{fontSize: '71px', lineHeight: '50px'}}>Останні новини</div>
+                            <ArrowCircleUpIcon sx={{fontSize: '65px', transform: 'rotate(45deg)'}}/>
+                        </Stack>
+                        <Divider color='black' sx={{borderBottomWidth: 4, marginBlock: '15px 30px'}} />
                         <Grid container spacing={3}>
                             {news.map(function(item) {
                                 return (
                                     <Grid item xs={12} sm={6} md={4}>
                                         <div>
                                             <img src={newsImage} width="100%"></img>
-                                            <Stack direction="row">
-                                                <Typography variant="body2" color="primary" style={{fontSize: '28px'}}>{item.title}</Typography>
-                                                <ArrowForwardIcon style={{fontSize: '45px'}}/>
+                                            <Stack direction="row" spacing={1}>
+                                                <Typography variant="body2" color="primary" style={{fontSize: '25px'}}>{item.title}</Typography>
+                                                <ArrowForwardIcon style={{fontSize: '25px', marginTop: '10px', transform: 'rotate(-45deg)'}}/>
                                             </Stack>
-                                            <Typography variant="body2" color="primary">12/12/2021</Typography>
-                                            <Typography variant="body2" color="primary" sx={{marginTop: '15px'}}>{item.text}</Typography>
-                                            <Divider color='black' sx={{borderBottomWidth: 4, marginTop: '10px'}} />
+                                            <Typography variant="body2" color="primary" fontWeight={'bold'}>{item.date}</Typography>
                                         </div>
                                     </Grid>
                                 )})}
+                        </Grid>
+                        <Divider color='black' sx={{borderBottomWidth: 4, marginBlock: '25px 30px'}} />
+                        <Grid container spacing={3}>
+                            <Grid item sm={12} md={8}>
+                                <img src={family} width="100%" style={{minWidth: '397px'}}></img>
+                            </Grid>
+                            <Grid item sm={12} md={4} sx={{alignSelf: 'center'}}>
+                                <Stack direction="row" spacing={2}>
+                                    <div style={{fontSize: '71px', lineHeight: '50px', marginBottom: '50px'}}>Історія</div>
+                                    <ArrowCircleUpIcon sx={{fontSize: '65px', transform: 'rotate(45deg)'}}/>
+                                </Stack>
+                                <Typography variant="body2" color="primary">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</Typography>
+                            </Grid>
+                        </Grid>
+                        <Divider color='black' sx={{borderBottomWidth: 4, marginBlock: '25px 30px'}} />
+                        <Grid container spacing={3}>
+                            <Grid item sm={12} md={4} sx={{alignSelf: 'center'}}>
+                                <Stack direction="row" spacing={2}>
+                                    <div style={{fontSize: '71px', lineHeight: '50px', marginBottom: '50px'}}>Колекції</div>
+                                    <ArrowCircleUpIcon sx={{fontSize: '65px', transform: 'rotate(45deg)'}}/>
+                                </Stack>
+                                <Typography variant="body2" color="primary">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</Typography>
+                            </Grid>
+                            <Grid item sm={12} md={8}>
+                                <img src={room} width="100%" style={{minWidth: '397px'}}></img>
+                            </Grid>
+                        </Grid>
+                    </div>
+                    <div className="bottom_grey_content">
+                        <Grid container spacing={3} sx={{alignSelf: 'center', marginTop: '25px', paddingBlock: '50px'}}>
+                            <Grid item sm={12} md={6}>
+                                <div style={{fontSize: '71px', lineHeight: '50px', marginTop: '20px'}}>Підтримай нас</div>
+                            </Grid>
+                            <Grid item sm={12} md={6}>
+                                <Typography variant="body2" color="primary">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</Typography>
+                                <Stack direction="row" spacing={2} sx={{marginBottom: '20px'}}>
+                                    <Typography variant="body2" color="primary" fontWeight={'bold'} marginTop={'20px'} style={{fontSize: '20px', textDecoration: 'underline'}}>Пожертвування</Typography>
+                                    <ArrowForwardIcon style={{fontSize: '25px', marginTop: '25px', transform: 'rotate(-45deg)'}}/>
+                                </Stack>
+                            </Grid>
+                        </Grid>
+                    </div>
+                    <div className="bottom_content" style={{paddingTop: '30px'}}>
+                        <Grid container spacing={3}>
+                            <Grid item sm={12} md={8}>
+                                <img src={franko} width="100%" style={{minWidth: '397px'}}></img>
+                            </Grid>
+                            <Grid item sm={12} md={4} sx={{alignSelf: 'center'}}>
+                                <Stack direction="row" spacing={2}>
+                                    <div style={{fontSize: '71px', lineHeight: '50px', marginBottom: '50px'}}>Наші програми</div>
+                                    <ArrowCircleUpIcon sx={{fontSize: '65px', transform: 'rotate(45deg)'}}/>
+                                </Stack>
+                                <Typography variant="body2" color="primary">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</Typography>
+                            </Grid>
                         </Grid>
                     </div>
                 </div>
