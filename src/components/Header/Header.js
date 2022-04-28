8b8419095e891672e0c63fbb589036dadee1d76b
MarinaZak <marinna27.01.2002@gmail.com>
2022-04-28 14:29:22 +0300

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,82 +5,72 @@ import Group from '../../images/Group.png'
 import Vector from '../../images/Vector.png'
 import Group2 from '../../images/logo.png'
 import Vector2 from '../../images/Vector2.png'
+import { useState, useEffect } from 'react';
+import {useLocation} from "react-router-dom" ;
 
 
 
 function Header() {
-    if (window.location.pathname=='/') {
-        return (
-            <div className='GMenu'>
-                <Grid container spacing={0} backgroundColor='rgba(0,0,0,0.6)' height='120px'>
-                    <Grid item xs={0} sm={1}></Grid>
-                    <Grid item xs={8} alignSelf='center' id='menuN'>
-                        <Grid container spacing={0} alignItems="center">
-                            <Grid item xs={4} md={4} id='foto'>
-                                <img src={Group2} alt={"Group"}/>
-                            </Grid>
-                            <Grid item xs={3} md={1}>
-                                <Typography className='nameMenu' variant="body2" color="secondary">Музей</Typography>
-                            </Grid>
-                            <Grid item xs={3} md={1}>
-                                <Typography className='nameMenu' variant="body2" color="secondary">Онлайн</Typography>
-                            </Grid>
-                            <Grid item xs={3} md={1}>
-                                <Typography className='nameMenu' variant="body2" color="secondary">Іван Франко</Typography>
-                            </Grid>
-                            <Grid item xs={3} md={1}>
-                                <Typography className='nameMenu' variant="body2" color="secondary">Освіта</Typography>
-                            </Grid>
-                            <Grid item xs={3} md={1}>
-                                <Typography className='nameMenu' variant="body2" color="secondary">Про нас</Typography>
-                            </Grid>
-                            <Grid item xs={3} md={1}>
-                                <Typography className='nameMenu' variant="body2" color="secondary">Крамниця</Typography>
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                    <Grid item xs={0} md={1}></Grid>
-                    <Grid item xs={2} alignSelf='center'>
-                        <Grid container spacing={8} alignItems="center">
-                            <Grid item xs={1} id='foto1'>
-                                <img src={Vector2} alt={"Vector"}/>
-                            </Grid>
-                            <Grid item xs={1} id='foto1'>
-                                <div className='lng' style={{color: 'white'}}>UKR/ENG</div>
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                </Grid>
-            </div>
-        )
-    }
+
+    const [backgroundColor, setBackgroundColor] = useState();
+    const [image1, setImage1] = useState();
+    const [image2, setImage2] = useState();
+    const [textColor, setTextColor] = useState();
+    
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        (async () => {
+            if (window.location.pathname=='/') {
+                setBackgroundColor('rgba(0,0,0,0.6)')
+                setImage1(Group2)
+                setImage2(Vector2)
+                setImage2(Vector2)
+                setTextColor('secondary')
+            }
+            else{
+                if (window.location.pathname.match(/video\/[^\/]+$/)!=null) {
+                    setBackgroundColor('rgba(28,28,28,0.5)')
+                    setImage1(Group2)
+                    setImage2(Vector2)
+                    setTextColor('secondary')
+                }
+                else{
+                    setBackgroundColor('white')
+                    setImage1(Group)
+                    setImage2(Vector)
+                    setTextColor('primary')
+                }
+            }
+        })();
+      }, [pathname]);
 
     return (
         <div className='GMenu'>
-            <Grid container spacing={0} backgroundColor='white' height='120px'>
+            <Grid container spacing={0} backgroundColor={backgroundColor} height='120px'>
                 <Grid item xs={0} sm={1}></Grid>
                 <Grid item xs={8} alignSelf='center' id='menuN'>
                     <Grid container spacing={0} alignItems="center">
                         <Grid item xs={4} md={4} id='foto'>
-                            <img src={Group} alt={"Group"}/>
+                            <img src={image1} alt={"Group"}/>
                         </Grid>
                         <Grid item xs={3} md={1}>
-                            <Typography className='nameMenu' variant="body2" color="primary">Музей</Typography>
+                            <Typography className='nameMenu' variant="body2" color={textColor}>Музей</Typography>
                         </Grid>  
                         <Grid item xs={3} md={1}>
-                            <Typography className='nameMenu' variant="body2" color="primary">Онлайн</Typography>
+                            <Typography className='nameMenu' variant="body2" color={textColor}>Онлайн</Typography>
                         </Grid>
                         <Grid item xs={3} md={1}>
-                            <Typography className='nameMenu' variant="body2" color="primary">Іван Франко</Typography>
+                            <Typography className='nameMenu' variant="body2" color={textColor}>Іван Франко</Typography>
                         </Grid>
                         <Grid item xs={3} md={1}>
-                            <Typography className='nameMenu' variant="body2" color="primary">Освіта</Typography>
+                            <Typography className='nameMenu' variant="body2" color={textColor}>Освіта</Typography>
                         </Grid>
                         <Grid item xs={3} md={1}>
-                            <Typography className='nameMenu' variant="body2" color="primary">Про нас</Typography>
+                            <Typography className='nameMenu' variant="body2" color={textColor}>Про нас</Typography>
                         </Grid>
                         <Grid item xs={3} md={1}>
-                            <Typography className='nameMenu' variant="body2" color="primary">Крамниця</Typography>
+                            <Typography className='nameMenu' variant="body2" color={textColor}>Крамниця</Typography>
                         </Grid>
                     </Grid>
                 </Grid>
@@ -88,10 +78,12 @@ function Header() {
                 <Grid item xs={2} alignSelf='center'>
                     <Grid container spacing={8} alignItems="center">
                         <Grid item xs={1} id='foto1'>
-                            <img src={Vector} alt={"Vector"}/>
+                            <img src={image2} alt={"Vector"}/>
                         </Grid>
                         <Grid item xs={1} id='foto1'>
-                            <div className='lng'>UKR/ENG</div>
+                            <Typography color={textColor}>
+                                <div className='lng'>UKR/ENG</div>
+                            </Typography>
                         </Grid>
                     </Grid>
                 </Grid>
