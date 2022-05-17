2166540a58f9ad8926b99cd6a4a5c918e6b531a0
Дмитро Гарасим <94192861+DmitroGarasym@users.noreply.github.com>
2022-05-17 22:57:15 +0300

    Update Support.js

--- src/components/Support/Support.js
+++ src/components/Support/Support.js
@@ -7,23 +7,46 @@ import React from "react";
 function Support() {
     var donationValue = 0;
 
-    const myDonationBtn = () => {
+    const [pressed, setPressed] = React.useState(false);
+
+    const myDonationBtn = (event) => {
         var anotherContainer = document.getElementById("anotherContainer");
-        if (anotherContainer.style.display === "none") {
-            anotherContainer.style.display = "block";
-        } else {
+        if (anotherContainer.style.display === "block") {
             anotherContainer.style.display = "none";
+        } else {
+            anotherContainer.style.display = "block";
         }
+        changeColor(event.currentTarget)
     };
 
     const setDonation = (event) => {
+        setPressed(false)
+
+        var buttonM1 = document.getElementById("buttonM1")
+        buttonM1.style.setProperty("background-color", "#ffffff", "important");
+        buttonM1.style.setProperty("color", "#1C1C1C", "important");
+
+        var anotherContainer = document.getElementById("anotherContainer");
+        anotherContainer.style.display = "none";
+
         donationValue = event.currentTarget.getAttribute('donationValue');
-        console.log(donationValue)
+
     };
 
     const updateDonationValue = (event) => {
         donationValue = event.currentTarget.value;
-        console.log(donationValue)
+
+    }
+
+    const changeColor = (button) => {
+        if (pressed) {
+            button.style.setProperty("background-color", "#ffffff", "important");
+            button.style.setProperty("color", "#1C1C1C", "important")
+        } else {
+            button.style.setProperty("background-color", "#1C1C1C", "important");
+            button.style.setProperty("color", "#ffffff", "important")
+        }
+        setPressed(!pressed)
     }
 
     const confirmBtn = () => {
@@ -36,11 +59,6 @@ function Support() {
         var commentContainer = document.getElementById("comment");
         var comment = commentContainer.value;
 
-        console.log("name: " + name);
-        console.log("mail: " + mail);
-        console.log("phone: " + phone);
-        console.log("comment: " + comment);
-        console.log("amount: " + donationValue);
 
         const requestOptions = {
             method: 'POST',
@@ -49,7 +67,7 @@ function Support() {
         };
 
         fetch('https://frankos-museum-backend.azurewebsites.net/api/donate', requestOptions)
-            .then(response => {if (response.status === 200) {return response.text();} else {throw new Error();}})
+            .then(response => {if (response.status == 200) {return response.text();} else {throw new Error();}})
             .then(data => {
                 document.getElementById("payButton").style.setProperty("background-color", "white", "important");
                 document.getElementById("payButton").innerHTML = data;
@@ -63,60 +81,74 @@ function Support() {
             <Grid container spacing={0}>
                 <Grid item xs={3}></Grid>
                 <Grid item xs={6} id='menuN'>
-                    <div style={{fontSize: '71px', lineHeight: '71px', marginInline: 'auto', marginTop: '30px', fontWeight: 'bold', textAlign: 'center'}}>Підтримати нас</div>
+                    <h1>Підтримати нас</h1>
                     <p className='text1'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. 
                         Velit officia consequat duis enim velit mollit. 
                         Exercitation veniam consequat sunt nostrud amet.
                     </p>
                     <div className='boxS1'>
-                        <div style={{fontSize: '25px', lineHeight: '25px', marginInline: 'auto', marginTop: '30px', fontWeight: 'bold', textAlign: 'center'}}>Вибери суму, яку готовий пожертвувати</div>
-                        <div className='supportButtons'>
-                            <Button
-                                className='buttonM'
-                                id='button1'
-                                donationValue={50}
-                                onClick={setDonation}
-                            >
-                            50 грн
-                            </Button>
-                            <Button
-                                className='buttonM'
-                                id='button2'
-                                donationValue={150}
-                                onClick={setDonation}
-                            >
-                            150 грн
-                            </Button>
-                            <Button
-                                className='buttonM'
-                                id='button3'
-                                donationValue={250}
-                                onClick={setDonation}
-                            >
-                            250 грн
-                            </Button>
-                            <Button
-                                className='buttonM'
-                                id='button4'
-                                donationValue={500}
-                                onClick={setDonation}
-                            >
-                            500 грн
-                            </Button>
-                            <Button
-                                className='buttonM1'
-                                onClick={myDonationBtn}
-                            >
-                            Інша сума
-                            </Button>
-                        </div>
+                        <h3>Вибери суму, яку готовий пожертвувати</h3>
                         <Grid container spacing={0}>
+                            <Grid item xs={1}></Grid>
+                            <Grid item xs={2}>
+                                <Button
+                                    className='buttonM'
+                                    id='button1'
+                                    donationValue={50}
+                                    onClick={setDonation}
+                                >
+                                50 грн
+                                </Button>
+                            </Grid>
+                            <Grid item xs={2}>
+                                <Button
+                                    className='buttonM'
+                                    id='button2'
+                                    donationValue={150}
+                                    onClick={setDonation}
+                                >
+                                150 грн
+                                </Button>
+                            </Grid>
+                            <Grid item xs={2}>
+                                <Button
+                                    className='buttonM'
+                                    id='button3'
+                                    donationValue={250}
+                                    onClick={setDonation}
+                                >
+                                250 грн
+                                </Button>
+                            </Grid>
+                            <Grid item xs={2}>
+                                <Button
+                                    className='buttonM'
+                                    id='button4'
+                                    donationValue={500}
+                                    onClick={setDonation}
+                                >
+                                500 грн
+                                    </Button>
+                            </Grid>
+                            <Grid item xs={2}>
+                                <Button
+                                    id='buttonM1'
+                                    className='buttonM1'
+                                    onClick={myDonationBtn}
+                                >
+                                Інша сума
+                                </Button>
+                            </Grid>
+                            <Grid item xs={1}></Grid>
+                        </Grid>
+                    
+                    <Grid container spacing={0}>
                             <Grid item xs={3}></Grid>
                             <Grid item xs={6}>
                                 <div className='another' id='anotherContainer'>
                                     <div className='line'></div>
                                     <div id='money3'>
-                                        <p style={{"margin": "16px 0 0 0"}}>Моя сума пожертви:</p>
+                                        <p style={{"margin": "16px 0 0 0"}}>Моя сума пожертви :</p>
                                         <p className='money2'>
                                             <TextField  id="money2"
                                                         defaultValue={"0"}
@@ -127,10 +159,10 @@ function Support() {
                                     </div>
                                     <div className='line1'></div>
                                 </div>
-                                <div className='boxS2'>Залиши інформацію про себе, щоб ми могли подякувати тобі за твою підтримку</div>
+                                <div className='boxS2'>Залиши інформаціяю про себе, щоб ми могли подякувати тобі за втою підтримку</div>
                                 <TextField id="name" className='textF' label="Ваше ім'я" variant="standard" />
                                 <TextField id="mail" className='textF' label="Ваш E-mail " variant="standard" />
-                                <TextField id="phone" className='textF' label="Ваш номер телефону (необов’язково)" variant="standard" />
+                                <TextField id="phone" className='textF' label="Ваш номер телфону (необов’язково)" variant="standard" />
                                 <TextField id="comment" className='textF' label="Коментар (необов’язково)" variant="standard" />
                                 <Button
                                     className='buttonM2'
@@ -141,13 +173,15 @@ function Support() {
                                 </Button>
                             </Grid>
                             <Grid item xs={3}></Grid>
-                        </Grid>
-                    </div>
+               
+                    </Grid> </div>
                 </Grid>
                 <Grid item xs={3}></Grid>
             </Grid>
+        
         </div>
-    );
+
+);
 }
 
 
