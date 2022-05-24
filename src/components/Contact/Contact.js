11b0f54ba6ede290ae0e93349e3f3bea8a50ed99
MarinaZak <marinna27.01.2002@gmail.com>
2022-05-24 21:46:36 +0300

    .

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,6 +4,7 @@ import timeC from '../../images/timeC.png'
 import locationC from '../../images/locationC.png'
 import contactC from '../../images/contactC.png'
 import arrivalC from '../../images/arrivalC.png'
+import { Stack } from '@mui/material';
 
 fetch('http://frankos-museum-backend.azurewebsites.net/contacts/')
   .then(response => response.json())
@@ -15,44 +16,44 @@ function Contact() {
         
     return (
         <Grid container spacing={0}>
-            <Grid item xs={3}></Grid>
+            <Grid item xs={2}></Grid>
             <Grid item xs={6} id='menuN'>
                 <div className='contM'>
                     <h1 className='igraSans'>Контакти</h1>
                     <div className='lineC'></div>
                 </div>
                 <div className='timeC'>
-                 <Grid container spacing={0}>
-                    <Grid item xs={1}>
+                 <Stack direction="row" spacing={4}>
+                    <Stack>
                         <img id='timeC' src={timeC} alt={"timeC"}/>
-                    </Grid>
-                    <Grid item xs={11}>
+                    </Stack>
+                    <Stack>
                         <h3 className='titleC'>Час</h3>
                         <div className='textInfo'>
                             {allcontacts.contact.time}
                         </div>
-                    </Grid>
-                </Grid>
+                    </Stack>
+                </Stack>
                 </div>
                 <div className='location'>
-                    <Grid container spacing={0}>
-                        <Grid item xs={1}>
-                            <img id='locationC' src={locationC} alt={"locationC"}/> 
-                        </Grid>
-                        <Grid item xs={11}>
+                 <Stack direction="row" spacing={2}>
+                    <Stack>
+                        <img id='locationC' src={locationC} alt={"locationC"}/> 
+                    </Stack>
+                        <Stack>
                             <h3 className='titleC'>Локація</h3>
                             <div className='textInfo'>
                             {allcontacts.contact.location}
                             </div>
-                        </Grid>
-                    </Grid>
+                        </Stack>
+                    </Stack>
                 </div>
-                <div className='contactWU'>
-                    <Grid container spacing={0}>
-                        <Grid item xs={1}>
-                            <img id='contactC' src={contactC} alt={"timeCcontactC"}/>
-                        </Grid>
-                        <Grid item xs={11}>
+                <div classStack='contactWU'>
+                 <Stack direction="row" spacing={2}>
+                    <Stack>
+                        <img id='contactC' src={contactC} alt={"timeCcontactC"}/>
+                    </Stack>
+                        <Stack>
                             <h3 className='titleC'>Зв’язок з нами</h3>
                             <div className='textInfo'>
                             {allcontacts.contact.contact_us}
@@ -60,15 +61,15 @@ function Contact() {
                             <div className='textInfo'>
                                 lviv_franko@ukr.net
                             </div>
-                        </Grid>
-                    </Grid>
+                        </Stack>
+                    </Stack>
                 </div>
                 <div className='arrival'>
-                    <Grid container spacing={0}>
-                        <Grid item xs={1}>
-                            <img id='arrivalC' src={arrivalC} alt={"arrivalC"}/>
-                        </Grid>
-                        <Grid item xs={11}>
+                 <Stack direction="row" spacing={2}>
+                    <Stack>
+                        <img id='arrivalC' src={arrivalC} alt={"arrivalC"}/>
+                    </Stack>
+                        <Stack>
                             <h3 className='titleC'>Доїзд:</h3>
                             <div className='textInfo'>
                             {allcontacts.contact.arrival}
@@ -79,8 +80,8 @@ function Contact() {
                             <div className='textInfo'>
                                 автобус № 16, маршрутне таксі № 13, 27 (зупинка Католицький університет, 900 м).
                             </div>
-                        </Grid>
-                    </Grid>
+                        </Stack>
+                    </Stack>
                 </div>
                 <div className='mapC'>
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2574.0763133836685!2d24.029390614636565!3d49.822230879393565!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473ae7f574262733%3A0x747c1f490c13827e!2z0JvRjNCy0L7QstGB0LrQuNC5INC90LDRhtC40L7QvdCw0LvRjNC90YvQuSDQu9C40YLQtdGA0LDRgtGD0YDQvdC-LdC80LXQvNC-0YDQuNCw0LvRjNC90YvQuSDQvNGD0LfQtdC5INCY0LLQsNC90LAg0KTRgNCw0L3QutC-!5e0!3m2!1sru!2sua!4v1651228788518!5m2!1sru!2sua" 
